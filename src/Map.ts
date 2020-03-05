474fd42146094ab9a8fcbc8ea0501527c16c7844
Faye Skeen <fskeen@gmail.com>
2020-03-04 20:46:34 -0800

    more setup, added faker library

---
+++ src/Map.ts
@@ -0,0 +1,1 @@
+// This is the class that will create the Map component
---
+++ src/User.ts
@@ -0,0 +1,11 @@
+// This is the class that will create the User component
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {}
+}
